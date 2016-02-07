fb10efc0a703dfd4ed1757bbeff4b700c776785d
Yosapol Jitrak <yosapoljittarak@hotmail.com>
2016-02-07 16:50:00 +0700

    Level 3 pass.

--- Assets/Scripts/LaserBullet.cs
+++ Assets/Scripts/LaserBullet.cs
@@ -1,36 +1,40 @@
 ﻿using System;
 using UnityEngine;
-using System.Collections;
 
-public class LaserBullet : MonoBehaviour {
+public class LaserBullet : MonoBehaviour
+{
 
-	public float Range;
-	public float Speed;
+    public float Range;
+    public float Speed;
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
 
-		// Move ();
-		// CheckRange ();
+        Move();
+        CheckRange();
 
-	}
-
-
-	// to move the bullet we calculate dY
-	// float deltaY = Speed * Time.deltaTime;
-	// this.transform.Translate (0, deltaY, 0);
-
-	void Move ()
-	{
-		throw new NotImplementedException();
-	}
+    }
 
 
-	// ah and when the object has left our warzone let's destroy it
-	// if (this.transform.position.y > Range)
-	// Destroy (this.gameObject);
+    // to move the bullet we calculate dY
+    // float deltaY = Speed * Time.deltaTime;
+    // this.transform.Translate (0, deltaY, 0);
 
-	void CheckRange(){
-		throw new NotImplementedException();
-	}
+    void Move()
+    {
+        float deltaY = Speed * Time.deltaTime;
+        transform.Translate(0f, deltaY, 0f);
+    }
+
+
+    // ah and when the object has left our warzone let's destroy it
+    // if (this.transform.position.y > Range)
+    // Destroy (this.gameObject);
+
+    void CheckRange()
+    {
+        if (transform.position.y > Range)
+            Destroy(gameObject);
+    }
 }
